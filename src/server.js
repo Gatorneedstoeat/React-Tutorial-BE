a966f7acca0fb3249be9241f51bbeae817240037
Sam Slosser <samuel.slosser@rescuedogdesign.com>
2020-02-19 15:33:30 -0500

    adding mongo db

--- src/server.js
+++ src/server.js
@@ -1,40 +1,114 @@
-import express from 'express';
+import express, { response } from 'express';
 import bodyParser from 'body-parser';
-import mysql from 'mysql';
+import {MongoClient} from 'mongodb';
 
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: []
-    },
-    'learn-node':{
-        upvotes: 0,
-        comments: []
-    },
-    'my-thoughts-on-resumes':{
-        upvotes: 0,
-        comments: []
-    }
-}
+// const articlesInfo = {
+//     'learn-react': {
+//         upvotes: 0,
+//         comments: []
+//     },
+//     'learn-node':{
+//         upvotes: 0,
+//         comments: []
+//     },
+//     'my-thoughts-on-resumes':{
+//         upvotes: 0,
+//         comments: []
+//     }
+// }
 const app = express();
+
 app.use(bodyParser.json());
 
-app.post('/api/articles/:name/upvotes',(req,res) => {
-    const articleName = req.params.name;
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!!`);
+const withDB = async (operations) => {
+    try{
+ 
+        // connect to mongo db
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true });
+        const db = client.db('my-blog');
+       
+        await operations(db);
+    
+        client.close();
+    }catch(error){
+        res.status(500).json({message: 'Error connecting to db', error});
+    }
+}
+app.get('/api/articles/:name', async (req, res) => {
+    //get parameter 
+    
+        withDB(async (db) => {
+            const articleName = req.params.name;
+            const articlesInfo =  await db.collection('articles').findOne({ name: articleName });
+            res.status(200).json(articlesInfo);
+        });
+    
+
+
 });
-app.post('/api/articles/:name/add-comment',(req,res) => {
-    //desctructing the request username and text values are known
-    const { username, text } = req.body;
-    const articleName = req.params.name;
-    articlesInfo[articleName].comments.push({username, text});
-    res.status(200).send(articlesInfo[articleName]);
+// manual method switched to function call above
+// app.get('/api/articles/:name', async (req, res) => {
+//     try{
+//         //get parameter 
+//         const articleName = req.params.name;
+//         // connect to mongo db
+//         const client = await MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true });
+//         const db = client.db('my-blog');
+    
+//         const articlesInfo = await db.collection('articles').findOne({name: articleName});
+//         res.status(200).json(articlesInfo);
+//         client.close();
+//     }catch(error){
+//         res.status(500).json({message: 'Error connecting to db', error});
+//     }
+
+// });
+app.post('/api/articles/:name/upvotes', async (req, res) => {
+    try {
+        //get parameter 
+        const articleName = req.params.name;
+        // connect to mongo db
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true });
+        const db = client.db('my-blog');
+
+        const articlesInfo = await db.collection('articles').findOne({ name: articleName });
+
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                upvotes: articlesInfo.upvotes + 1,
+            }
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleInfo);
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: "Error connecting to db", error });
+    }
 });
+// app.post('/api/articles/:name/upvotes',(req,res) => {
+//     const articleName = req.params.name;
+//     articlesInfo[articleName].upvotes += 1;
+//     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!!`);
+// });
+// app.post('/api/articles/:name/upvotes',(req,res) => {
+//     const articleName = req.params.name;
+//     articlesInfo[articleName].upvotes += 1;
+//     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!!`);
+// });
+// app.post('/api/articles/:name/add-comment',(req,res) => {
+//     //desctructing the request username and text values are known
+//     const { username, text } = req.body;
+//     const articleName = req.params.name;
+//     articlesInfo[articleName].comments.push({username, text});
+//     res.status(200).send(articlesInfo[articleName]);
+// });
+
+
 app.get('/hello', (request,response) => response.send("hello!"));
 app.get('/hello/:name', (req,res) => res.send(`Hello you stud ${req.params.name}`))
 app.post('/hello', (req,res) => res.send(`Hello ${req.body.name} you stud!`));
 
+//testing mysql
 app.get('/test',  (req, res) => {
     var connection = mysql.createConnection({
         host: 'localhost',
